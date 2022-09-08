9b3f5874175f2e324b6df677f59431abe56c4206
devredemption <redemptionjonathan@yahoo.com>
2022-09-08 19:54:34 +0100

    The puts() function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*
+* Printing an output without using the printf()
+*
+*/
+
+int main()
+{
+  puts("Programming is like building a multilingual puzzle\n");
+  return (0);
+}

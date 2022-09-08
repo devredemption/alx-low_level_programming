fe89b9ed5727b9520d5a0952f04127f4e0d3160b
devredemption <redemptionjonathan@yahoo.com>
2022-09-08 20:14:06 +0100

    The puts()
      Using betty-style linter to pass this task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-/**
-*
-* Printing an output without using the printf()
-*
-*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

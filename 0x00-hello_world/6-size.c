b3da41b523d4c0b78cb28f147f9084ae7e77d889
devredemption <redemptionjonathan@yahoo.com>
2022-09-08 22:07:07 +0100

    Datetype size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
-/*
- * main - Entry
- *
- * Return: 0 success
- * */
+
+/**
+  * main - Entry point
+  *
+  * Return: 0 on success
+  */
 int main(void)
 {
-    int i;
-    char c;
-    long l;
-    long long ll;
-    float f;
-    printf("Size of a char: %d\n",sizeof(c));
-    printf("Size of an int: %d\n",sizeof(i));
-    printf("Size of a long int: %d\n",sizeof(l));
-    printf("Size of an long long int : %d\n",sizeof(ll));
-    printf("Size of an float: %d\n",sizeof(f));
-    return (0);
+	int i;
+	char c;
+	long l;
+	long long ll;
+	float f;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
+	printf("Size of an int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(ll));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
+	return (0);
 }
